d7767c8863cf88793e6eee6bb1c8ecda63af5797
saborlatinony <info@saborlatinony.com>
2024-06-04 22:10:35 -0400

    Update scrape_eventbrite.py

--- scrape_eventbrite.py
+++ scrape_eventbrite.py
@@ -10,18 +10,23 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 # Example data extraction (needs to be customized based on actual page structure)
 events = []
+event_counter = 1
 for event in soup.find_all('div', class_='search-event-card-wrapper'):
     title = event.find('div', class_='eds-event-card__formatted-name--is-clamped').text.strip()
-    date = event.find('div', class_='eds-event-card-content__sub-title').text.strip()
-    location = event.find('div', class_='card-text--truncated__one').text.strip()
     link = event.find('a', class_='eds-event-card-content__action-link')['href']
+    description = event.find('div', class_='eds-event-card-content__sub-title').text.strip()
+    image_link = event.find('img', class_='card-image__img')['src']
+
+    # Generate a unique ID for each event
+    unique_id = f"event_{event_counter}"
+    event_counter += 1
 
     events.append({
-        "title": title,
-        "date": date,
-        "location": location,
-        "description": "",  # Add description extraction logic if available
-        "link": link
+        "$id": unique_id,
+        "$title": title,
+        "$link": link,
+        "$image_link": image_link,
+        "$description": description
     })
 
 # Save data to JSON file
